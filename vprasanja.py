df05929b763c9c27a6795b5246ffe9e227ec009a
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2020-07-02 19:44:02 +0200

    vprasanja za kviz

---
+++ vprasanja.py
@@ -0,0 +1,41 @@
+#moram napisati še primere odgovorov
+slovar = {"Kakšna je dolžina induvidualne sestave?":" 1:15 - 1:30",
+"Kakšen je trenutni program za induvidualne tekmovake?":"obroč, žoga, kiji, trak",
+"Kakšna je dolžina skupinske sestave?":"2:15 - 2:30",
+"Kakšen je program za skupinske sestave?":"sestava z enim tipom rekvizita in z dvema tipoma rekvizitov",
+"Kolikšen je odbitek za predolgo glasbo":"0.05",
+"Kako dolg je lahko uvod glasbe?":"4 sekunde",
+"Kdo odbije odbitek za predolg glasbeni uvod?":"sodnice za artistiko",
+"Kolikšen je odbitek za predolg glasbeni uvod?":"0.30",
+"Kolikšen je odbitek za neprimerni glasbo (npr. sirene, motor, itd.)?":"0.50",
+"Koliko glasb s petjem ima lahko tekmovalka?":"2",
+"Kdo odbije odbitke če glasba ni v skladu s pravilnikom?":"sodnica koordinatorka",
+"Kolikšen je odbitek če tekmovalka uporabi dodatno glasbo s petjem?":"1.00",
+"Kdo sodi težine s telesom in plesne korake?":"D1, D2",
+"Kdo sodi rizike in težine z rekvizitom?":"D3, D4",
+"Kdo sodi artistične komponente sestave?":"E1, E2",
+"Kdo sodi izvedbene napake?":"E3, E4, E5, E6",
+"Na kateri del ocene se lahko pritoži trener?":"D",
+"Kolikšen je odbitek za prestop?":"0.30",
+"Kolikšen je odbitek za neustrezen rekvizit?":"0.50",
+"kolikšen je odbitek, če tekmovani dres ne ustreza zahtevam?":"0.30",
+"Ali lahko tekmovalke med sestavo komunicirajo?":"ne",
+"Ali lahko trener daje napotke tekmovalki med izvajanjem sestave?":"ne",
+"Kolikšen je odbitek, če trenerka kaže vajo med tem ko dekleta tekmujejo?":"0.50",
+"Kolikšen je odbitek, če ima tekmovalka nekožnate bandaže?":"0.30"
+}
+
+#pod prvo možnostjo je naveden pravilni odgovor
+vprasanja_multiple_izbire = {
+    'Koliko je vredna težina na sliki1?':['0.1', '0.2', '0.3'],
+    'Koliko je vredna težina na sliki2?':['0.2', '0.1', '0.3'],
+    'Koliko je vredna težina na sliki3?':['0.2', '0.3', '0.4'],
+    'Koliko je vredna težina na sliki4?':['0.3', '0.2', '0.4'],
+    'Koliko je vredna težina na sliki5?':['0.3', '0.4', '0.5'],
+    'Koliko je vredna težina na sliki6?':['0.4', '0.5', '0.6'],
+    'Koliko je vredna težina na sliki7?':['0.2', '0.3', '0.4'],
+    'Koliko je vredna težina na sliki8?':['0.3', '0.4', '0.2'],
+    'Koliko je vredna težina na sliki9?':['0.4', '0.3', '0.5'],
+    'Koliko je vredna težina na sliki10?':['0.5', '0.6', '0.4'],
+    
+}
