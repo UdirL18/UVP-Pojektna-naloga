bcac7090165604574e43f338b8cc112d120744e2
UdirL18 <lucija.udir@student.fmf.uni-lj.si>
2020-07-21 13:07:57 +0200

    ugibaj sedaj zazna ko pridemo do vprasanj tip_2.

--- model.py
+++ model.py
@@ -2,15 +2,17 @@ import random
 from vprasanja import slovar, vprasanja_multiple_izbire1, riziki 
 
 STEVILO_DOVOLJENIH_NAPAK = 5 #potem spremeni na 3
-STEVILO_PRAVILNIH = 12
-KVIZ_MULTIPLE = 4
-KVIZ_RIZIKI = 8
+STEVILO_PRAVILNIH = 9
+STEVILO_KVIZ_MULTIPLE = 4
+STEVILO_KVIZ_RIZIKI = 8
 PRAVILEN_ODGOVOR = "+"
 NI_ODGOVORA = "0"
 NAPACEN_ODGOVOR = "-"
 ZMAGA = "W"
 PORAZ = "X"
 ZACETEK = "S"
+KVIZ_MULTIPLE = "M"
+KVIZ_RIZIKI = "R"
 
 class Igra:
     def __init__(self, st_vprasanj):
@@ -21,13 +23,13 @@ class Igra:
         self.vprasanja_riziki = random.sample(list(riziki), 1) #želim da bo na eno igro samo en video (vrne npr [1])
 
     def trenutno_vprasanje(self):
-        if self.pravilni_odgovori >= KVIZ_RIZIKI:
+        if self.pravilni_odgovori >= STEVILO_KVIZ_RIZIKI:
             # želim da izpiše vseh 5 (oz 4) vprašanja
             vpr_riz = int(self.vprasanja_riziki[0]) # vrne npr 1
             return riziki.get(vpr_riz) # vrne {"tip": "tip_2", "vprasanje": [{'vpr':'', 'odg': [odg]}, {:[]}, ], "mozni_odg": [], "video": "https"}
-        if self.pravilni_odgovori in range(KVIZ_MULTIPLE, KVIZ_RIZIKI):
+        if self.pravilni_odgovori in range(STEVILO_KVIZ_MULTIPLE, STEVILO_KVIZ_RIZIKI):
             vpr_mul = self.vprasanja_mul[self.trenutno_vprasanje_idx] #vrne npr 18
-            return vprasanja_multiple_izbire1.get(vpr_mul) #{'tip': 'tip_1', 'vprasanje': 'Koliko je vredna težina na sliki 18?', 'odgovor': 0.4, 'mozni_odg': [0.4, 0.5, 0.6], 'slika': 'http'}
+            return vprasanja_multiple_izbire1.get(vpr_mul) #{'tip': 'tip_1', 'vprasanje': 'Koliko je vredna težina na sliki 18?', 'odgovor': '0.4', 'mozni_odg': [0.4, 0.5, 0.6], 'slika': 'http'}
         else:
             vpr_0 = self.vprasanja[self.trenutno_vprasanje_idx] #vrne npr 4
             return slovar.get(vpr_0) #{'tip': 'tip_0', 'vprasanje': '?', 'odgovor': ''}
@@ -38,27 +40,36 @@ class Igra:
     def stevilo_pravilnih(self):
         return self.pravilni_odgovori
 
+    def tip_2(self):
+        return self.pravilni_odgovori == STEVILO_KVIZ_RIZIKI
+
+    def tip_1(self):
+        return self.pravilni_odgovori in range(STEVILO_KVIZ_MULTIPLE, STEVILO_KVIZ_RIZIKI)
+
     def zmaga(self):
-        return self.pravilni_odgovori >= STEVILO_PRAVILNIH
+        return self.pravilni_odgovori == STEVILO_PRAVILNIH
     
     def poraz(self):
         return self.stevilo_napacnih() > STEVILO_DOVOLJENIH_NAPAK
 
     def ugibaj(self, odgovor):
         if odgovor == "":
-            return NI_ODGOVORA
-    # TO NI KUL :( 
-        if self.pravilni_odgovori >= KVIZ_RIZIKI:
-            seznam_vpr = self.trenutno_vprasanje.get('vprasanje') # [{'vpr':'','odg':[]}, {vpr:odg}, ...]
+            return NI_ODGOVORA #vrne "0"
+        if self.pravilni_odgovori >= STEVILO_KVIZ_RIZIKI:
+            seznam_vpr = self.trenutno_vprasanje().get('vprasanje') # [{'vpr':'','odg':[]}, {vpr:odg}, ...]
             for slovar_vpr in seznam_vpr:
                 pravilen_odgovor = slovar_vpr.get("odg") 
-        if self.pravilni_odgovori in range(KVIZ_MULTIPLE, KVIZ_RIZIKI): 
+        if self.pravilni_odgovori in range(STEVILO_KVIZ_MULTIPLE, STEVILO_KVIZ_RIZIKI): 
             pravilen_odgovor = self.trenutno_vprasanje().get("odgovor") # vrne npr 0.4
         else:    
             pravilen_odgovor = self.trenutno_vprasanje().get("odgovor")
         self.trenutno_vprasanje_idx += 1
         if odgovor == pravilen_odgovor:
             self.pravilni_odgovori += 1
+            if self.tip_2(): 
+                return KVIZ_RIZIKI
+            elif self.tip_1(): 
+                return KVIZ_MULTIPLE                            
             if self.zmaga(): 
                 return ZMAGA
             return PRAVILEN_ODGOVOR
@@ -79,15 +90,15 @@ class Kviz:
         if self.igre == {}:
             return 0
         else:
-            return max(self.igre.keys()) + 1
+            return max(self.igre.keys()) + 1 #dict_keys([1, 2]), max vrne 2, prost_id_igre vrne 3
 
     def nova_igra(self):
-        igra = nova_igra()
-        id_igre = self.prost_id_igre()
-        self.igre[id_igre] = (igra, ZACETEK)
+        igra = nova_igra() #Igra(st_vprasanj)
+        id_igre = self.prost_id_igre() 
+        self.igre[id_igre] = (igra, ZACETEK) #igre[id_igre] vrne vrednosti pri tem ključu 
         return id_igre
 
     def ugibaj(self, id_igre, odgovor):
-        igra = self.igre[id_igre][0]
+        igra = self.igre[id_igre][0] 
         stanje = igra.ugibaj(odgovor)
-        self.igre[id_igre] = (igra, stanje)
+        self.igre[id_igre] = (igra, stanje) # stanje "R", "M", "W", "X" in "0", "-", "+"
